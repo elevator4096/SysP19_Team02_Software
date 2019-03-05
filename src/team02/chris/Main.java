c8c90e7485d3d01e362c8313167b73dbc97f09f8
Chris <chris.ruettimann@gmx.net>
2019-03-05 14:25:38 +0100

    Everything updated

--- src/team02/chris/Main.java
+++ src/team02/chris/Main.java
@@ -1,7 +1,11 @@
-
+/**
+ * @Author Chris
+ * @version 2019.03.05
+ */
 package team02.chris;
 
 import ch.ntb.inf.deep.runtime.ppc32.Task;
+
 
 
 /**
@@ -28,7 +32,68 @@ public class Main extends Task
     }
 
 
+    public Main()
+    {
 
+    }
 
+    public void action()
+    {
+        if (Konstanten.DEBUG)
+        {
+
+        }
+
+        switch (zustand)
+        {
+            case SETUP:
+            {
+
+                break;
+            }
+
+            case SPIEL_BEGINNT:
+            {
+
+                break;
+            }
+            case ROB_HAT_KEIN_BALL:
+            {
+
+                break;
+            }
+            case ROB_FAEHRT:
+            {
+
+                break;
+            }
+            case ROB_HAT_BALL:
+            {
+
+                break;
+            }
+            case ROB_POSITION_ERREICHT:
+            {
+
+                break;
+            }
+            case KORB_WURF:
+            {
+
+                break;
+            }
+            case KURZER_WURF:
+            {
+
+                break;
+            }
+            case LANGER_WURF:
+            {
+
+                break;
+            }
+        }
+
+    }
 
 }
