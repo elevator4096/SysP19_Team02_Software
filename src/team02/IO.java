27b2e4998e4cd09bbe2758530289b5c5db97b189
elevator4000 <loris-graf@bluewin.ch>
2019-03-12 15:14:03 +0100

    sharp

--- src/team02/IO.java
+++ src/team02/IO.java
@@ -5,6 +5,7 @@
 package team02;
 
 import ch.ntb.inf.deep.runtime.mpc555.driver.MPIOSM_DIO;
+import ch.ntb.inf.deep.runtime.mpc555.driver.QADC_AIN;
 import ch.ntb.inf.deep.runtime.mpc555.driver.TPU_FQD;
 import ch.ntb.inf.deep.runtime.mpc555.driver.TPU_PWM;
 
@@ -14,6 +15,9 @@ public interface IO {
 	MPIOSM_DIO IN_Taster1 				= new MPIOSM_DIO( 5, false);
 	MPIOSM_DIO IN_Taster2				= new MPIOSM_DIO( 6, false);
 	MPIOSM_DIO IN_BallDetektor			= new MPIOSM_DIO( 7, false);
+	
+	QADC_AIN   ADC_B					= new QADC_AIN();
+	int 	   PIN_LeftSharpChannel		= 0;
 	
 	//Output
 	MPIOSM_DIO OUT_Test_Led				= new MPIOSM_DIO(10, true );
---
+++ src/team02/Sharp.java
@@ -0,0 +1,16 @@
+package team02;
+
+public class Sharp {
+	
+	public Sharp()
+	{
+		IO.ADC_B.init(false);
+	}
+	
+	public int readLeft()
+	{	
+		return IO.ADC_B.read(false, 0);
+	}
+	
+	
+}
