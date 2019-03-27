a961163f6bacc2dbb196f8efeb6b98f9b5ce2fcb
Chris <chris.ruettimann@gmx.net>
2019-03-27 14:41:05 +0100

    Sachen aufgeraeumt

--- src/team02/beispiele/WurfSystem.java
+++ src/team02/beispiele/WurfSystem.java
@@ -2,7 +2,7 @@
  * @author loris
  * @version 2019.03.17
  */
-package team02.vorlagen;
+package team02.beispiele;
 
 import team02.IO;
 import team02.Konstanten;
@@ -97,5 +97,57 @@ public class WurfSystem
 		d = ((double)(i))/100;
 		d = d*IO.PERIOD_WurfZyl;
 		IO.PWM_WurfZylinder.update((int)d);
+	}
+	
+	
+	public void action() {
+		
+		/*
+		//Testprogramm
+		if (timer1.expired())
+		{
+			zustand++;
+			switch(zustand) {
+			case 1:
+				pwm.setWurfZylinderPWM(30);
+				timer1.set(5000);
+				break;
+			case 2:
+				if(IO.ballDetektor.get())
+				{
+					IO.wurfMagnet.set(true);
+					timer1.set(1000);
+				}
+				IO.OUT_Magnet_Wand.set(true);
+				timer1.set(1000);
+
+				break;
+			case 3:
+				IO.OUT_Magnet_Wand.set(false);
+				pwm.setWurfZylinderPWM(80);
+				zustand = 0;
+				timer1.set(10000);
+				break;
+			}
+		}
+		*/
+		
+	}
+	
+	static 
+	{
+		/*
+		// Einfahrzeit ca 12 Sekunden
+		Task t = new WurfSystem(); // Task erzeugen
+		t.period = 500; // Task-Periode festlegen
+		Task.install(t); // Task installieren
+		
+		pwm = new PWM();
+		timer1 = new Timer();
+		timerv
+		zustand = 0;
+		pwm.setWurfZylinderPWM(80);
+		*/
+		
 	}
 }
