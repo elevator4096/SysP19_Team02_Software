415a383063e36dcee5709b62bde9dbd5bb8bce86
elevator4000 <loris-graf@bluewin.ch>
2019-03-31 22:59:41 +0200

    Update Test_BewegungsSystem.java

--- src/team02/testklassen/Test_BewegungsSystem.java
+++ src/team02/testklassen/Test_BewegungsSystem.java
@@ -1,15 +1,41 @@
 package team02.testklassen;
 
-public class Test_BewegungsSystem {
+import team02.IO;
 
+public class Test_BewegungsSystem {
+	public static long lastTime = System.currentTimeMillis();  
+	public static int counter=0; 
+	
 	public static void test()
 	{
+		if (System.currentTimeMillis() > lastTime+5000)
+		{
+			counter++;
+			switch(counter)
+			{
+			case 1:
+				folgeLinieVorwaerts();
+				break;
+			case 2:
+				folgeLinieRueckwaerts();
+				break;
+			case 3:
+				break;
+			}
+			
+			lastTime = System.currentTimeMillis();
+		}
 		
 	}
 	
-	public static void linieFolgen()
+	public static void folgeLinieVorwaerts()
 	{
-		
+		IO.bewegungsSystem.folgeLinie(true);
+	}
+	
+	public static void folgeLinieRueckwaerts()
+	{
+		IO.bewegungsSystem.folgeLinie(false);
 	}
 	
 	
