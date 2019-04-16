8922569bdf3fd9591a4eabdc0c3b144e77be1a4a
Chris <chris.ruettimann@gmx.net>
2019-04-16 14:28:15 +0200

    Test aufgeräumt

--- src/team02/testklassen/Test_Task.java
+++ src/team02/testklassen/Test_Task.java
@@ -33,50 +33,9 @@ public class Test_Task extends Task implements IO, Systeme, Konstanten
 
     public void action()
     {
-    	/*
-    	OUT_LED1.set(!OUT_LED1.get());
-    	debugSystem.println("------");
-        gegnerSystem.test();
-        debugSystem.update();
-        */
-    	
-    	//DIG_IO.test();
-    	
-    	
-    	//Test_LinienSensoren.test();
-    	//debug.println(debug.read());
-    	//Test_Sharp.test();
-    	//Test_BewegungsSystem.folgeLinieRueckwaerts();
-    	//Test_Fahren.vorwaerts();
-
-    
     	wlanSystem.setOwnState(ZustandWifi.FAHREN);
     	wlanSystem.update();
     	debug.println(wlanSystem.getPartnerState());
-
-    	//Test_Motor.setLeftSpeed0();
-    	
-    	//Test_Motor.setLeftSpeed50();
-    	
-    	//wlanSystem.setOwnState(2);
-    	//wlanSystem.update();
-    	//debug.println(wlanSystem.getPartnerState());
-    	//debug.println("Hallo");
-    	
-    	//Test_Fahren.dreheGUZ(); 		//ok!
-    	//Test_Fahren.dreheUZ();  		//ok!
-    	//Test_Fahren.vorwaerts();  	//ok!
-    	//Test_Fahren.rueckwaerts();  	//ok!
-    	
-    	Fahren.kurveFahren(Konstanten.WHEEL_DISTANCE, 0.1);
-    	
-    	/*
-    	wlanSystem.setOwnState(2);
-    	wlanSystem.update();
-    	debug.println(wlanSystem.getPartnerState());
-    	*/
-    	
-    	//IO.MOTOR_links.updateSpeed(0.1);
     }
 
 }
