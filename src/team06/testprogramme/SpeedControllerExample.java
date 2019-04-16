b7c97d74bc4621764c0c601b065af56093f3f149
sednarb <48724039+sednarb@users.noreply.github.com>
2019-04-16 11:47:22 +0200

    SpeedController

--- src/team06/testprogramme/SpeedControllerExample.java
+++ src/team06/testprogramme/SpeedControllerExample.java
@@ -18,13 +18,19 @@ public class SpeedControllerExample extends Task {
 	protected static final float tn = 0.01f;
 	/* Ticks per rotation from encoder datasheet */
 	protected static final int ticksPerRotation = 64;
-	protected static final float gearRatio = 3249f / 196f;
+	protected static final float gearRatio = 1;//3249f / 196f
 	protected static final float motorVoltage = 12f;
 	static SpeedController4DCMotor motor;
+	
+	
+	
+	public static int x =0;
 
 	public void action() {
+
+	
 		motor.run();
-		System.out.println(motor.getActualSpeed());
+		
 	}
 
 	static {
@@ -33,16 +39,19 @@ public class SpeedControllerExample extends Task {
 				motorVoltage, gearRatio, kp, tn);
 		// Set desired speed
         // full turn once per second
-		motor.setDesiredSpeed((float)0);
+		
+		motor.setDesiredSpeed(0);
+		
+		
         // Initialize task
 		Task t = new SpeedControllerExample();
 		t.period = (int) (ts * 1000);
 		Task.install(t);
 
-		SCI sci1 = SCI.getInstance(SCI.pSCI1);
+		/*SCI sci1 = SCI.getInstance(SCI.pSCI1);
 		sci1.start(9600, SCI.NO_PARITY, (short) 8);
 
 		System.out = new PrintStream(sci1.out);
-		System.out.println("static start");
+		System.out.println("static start");*/
 	}
 }
