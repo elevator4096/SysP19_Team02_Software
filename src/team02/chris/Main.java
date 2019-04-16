257686f913423f39b3a3da80462e5924a2ab1ef0
Chris <chris.ruettimann@gmx.net>
2019-04-16 08:59:03 +0200

    Reihenfolge Main geändert

--- src/team02/chris/Main.java
+++ src/team02/chris/Main.java
@@ -280,10 +280,9 @@ public class Main extends Task implements IO, Systeme
     private void update()
     {
     	IO.debug.update();
-    	
+        Systeme.wlanSystem.update();
         Systeme.bewegungsSystem.update();
         Systeme.gegnerSystem.update();
-        Systeme.wlanSystem.update();
         Systeme.wurfSystem.update();
     }
 }
