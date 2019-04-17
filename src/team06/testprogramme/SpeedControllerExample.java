b3050520f563725b9d33e44831d6b95de3e8707a
sednarb <48724039+sednarb@users.noreply.github.com>
2019-04-17 21:53:44 +0200

    Speedcontroller test

--- src/team06/testprogramme/SpeedControllerExample.java
+++ src/team06/testprogramme/SpeedControllerExample.java
@@ -14,47 +14,65 @@ public class SpeedControllerExample extends Task {
 	protected static final int TPU_FQD_A = 6;
 	/* Controller parameters */
 	protected static final float ts = 0.001f;
-	protected static final float kp = 1f;
-	protected static final float tn = 0.01f;
+	protected static final float kp = 0.0001f;             // Fahrmotor Standard // Wurfmotor 0.0001f;
+	protected static final float tn = 0.0051f;            // Testboard 0.01   // Fahrmotor 0.008f  // Wurfmotor 0.0051f
 	/* Ticks per rotation from encoder datasheet */
-	protected static final int ticksPerRotation = 64;
-	protected static final float gearRatio = 3249f / 196f;
+	protected static final int ticksPerRotation = 32;    // Testboard 64     // Fahrmotor 256      //  Wurfmotor 32
+	protected static final float gearRatio = 1f;    //Testboard 3249f   // Fahrmotor 196f     // 91f/1f //   Wurfmotor  1f / 1f
 	protected static final float motorVoltage = 12f;
 	static SpeedController4DCMotor motor;
-	
-	
-	
-	
-	public static int x =0;
+
+	public SpeedControllerExample() {
+
+		System.out.println("Speedcontroller gestartet");
+
+	}
 
 	public void action() {
 
-	
 		motor.run();
-		//System.out.println(motor.getActualPosition());
-		//System.out.println("static start");
+		// System.out.println(motor.getActualPosition());
+		// System.out.println("static start");
+	}
+
+	public int gibweg() {
+		return (int) (motor.getActualPosition() / (2 * Math.PI));
+	}
+	
+	public int gibgeschwindigkeit()
+	{
+		return (int)(motor.getActualSpeed());
+	}
+	
+	public static void drehzahl()
+	{
+	motor.setDesiredSpeed((float)(200*Math.PI));   // 150*pi == 5400 //  200*pi max --> 6000
+	}
+	
+	public static void zwanzig()
+	{
+	motor.setDesiredSpeed((float)(20*Math.PI));   // 150 == 5400
+	}
+	
+	public static void nul()
+	{
+	motor.setDesiredSpeed((float)(0*Math.PI));   // 150 == 5400
 	}
 
 	static {
-        // Create controller
+		// Create controller
 		motor = new SpeedController4DCMotor(ts, TPU_PWM_CH0, TPU_PWM_CH1, TPU_A, TPU_FQD_A, TPU_A, ticksPerRotation,
 				motorVoltage, gearRatio, kp, tn);
 		// Set desired speed
-        // full turn once per second
-		
-		motor.setDesiredSpeed((float)(Math.PI));
-		//motor.setDesiredSpeed((float)(0));
-		
-		
-        // Initialize task
+		// full turn once per second
+
+		 //motor.setDesiredSpeed((float)(50*Math.PI));
+		 motor.setDesiredSpeed((float) (0));
+
+		// Initialize task
 		Task t = new SpeedControllerExample();
 		t.period = (int) (ts * 1000);
 		Task.install(t);
 
-		SCI sci1 = SCI.getInstance(SCI.pSCI1);
-		sci1.start(9600, SCI.NO_PARITY, (short) 8);
-
-		System.out = new PrintStream(sci1.out);
-		System.out.println("static start");
 	}
 }
--- src/team06/testprogramme/Test.java
+++ src/team06/testprogramme/Test.java
@@ -10,33 +10,35 @@ import team06.aurelia.Main;
 
 public class Test extends Task {
 
+	SpeedControllerExample controller;
+
 	public Test() {
+
+		controller = new SpeedControllerExample();
 		System.out.println("Main Konstruktor gestartet");
 	}
 
-	public static void schreiben() {
-		System.out.println("schreiben");
-	}
-
 	public void action() {
-		System.out.println("test");
+		System.out.print("Umdrehungen  >");
+		System.out.print(controller.gibweg());
+		System.out.print("        Geschwindigkeit  >");
+		System.out.println(controller.gibgeschwindigkeit());
+		
+		
 
 	}
 
 	static {
 
-		Task task = new Main();
-		task.period = Variablen.TASK_PERIOD;
+		Task task = new Test();
+		task.period = 500;
 		Task.install(task);
 
-		{
-			SCI sci1 = SCI.getInstance(SCI.pSCI1);
-			sci1.start(9600, SCI.NO_PARITY, (short) 8);
-			// Hook SCI1.out on System.out
-			System.out = new PrintStream(sci1.out);
-		}
-
-		System.out.println("test test test test");
+		SCI sci1 = SCI.getInstance(SCI.pSCI1);
+		sci1.start(9600, SCI.NO_PARITY, (short) 8);
+		// Hook SCI1.out on System.out
+		System.out = new PrintStream(sci1.out);
+		System.out.println("static start");
 
 	}
 
