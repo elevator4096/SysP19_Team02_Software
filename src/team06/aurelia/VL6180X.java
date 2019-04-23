8a85499daaa8bddc524a5c18eaac9a2a7366d06a
salaur00 <48723898+salaur00@users.noreply.github.com>
2019-04-23 15:07:09 +0200

    Update VL6180X.java

--- src/team06/aurelia/VL6180X.java
+++ src/team06/aurelia/VL6180X.java
@@ -10,8 +10,9 @@ public class VL6180X {
 	private VL6180X_SC18IS600 VL6180X;
 
 	public VL6180X() {
-		VL6180X = new VL6180X_SC18IS600 (2);
-		VL6180X.getSingleRangeValue(5);
+		VL6180X = new VL6180X_SC18IS600 (2); 	// Anzahl Sensoren (int)
+		// returns Distanz in mm
+		VL6180X.getSingleRangeValue(5);			// Slave Device Address (int)
 
 	}
 }
