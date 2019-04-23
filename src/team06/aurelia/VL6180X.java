2b6c38477d21d37e64f69801a4e53f409c423a5d
salaur00 <48723898+salaur00@users.noreply.github.com>
2019-04-23 14:55:02 +0200

    Create VL6180X.java
    Klasse für ToF VL6180X

---
+++ src/team06/aurelia/VL6180X.java
@@ -0,0 +1,17 @@
+package team06.aurelia;
+
+import java.lang.Object;
+import ch.ntb.inf.deep.runtime.ppc32.Task;
+import ch.ntb.inf.deep.runtime.mpc555.driver.VL6180XAddr;
+import ch.ntb.inf.deep.runtime.mpc555.driver.VL6180X_SC18IS600;
+
+public class VL6180X {
+
+	private VL6180X_SC18IS600 VL6180X;
+
+	public VL6180X() {
+		VL6180X = new VL6180X_SC18IS600 (2);
+		VL6180X.getSingleRangeValue(5);
+
+	}
+}
