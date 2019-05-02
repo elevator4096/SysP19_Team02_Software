f641cb2b4358838fb406cb1b4dbad32a690026d7
sednarb <48724039+sednarb@users.noreply.github.com>
2019-05-02 18:33:38 +0200

    Test M3

--- src/team06/phil/Test_Main_M3.java
+++ src/team06/phil/Test_Main_M3.java
@@ -5,6 +5,7 @@ import java.io.PrintStream;
 import ch.ntb.inf.deep.runtime.mpc555.driver.SCI;
 import ch.ntb.inf.deep.runtime.ppc32.Task;
 import team06.aurelia.Main;
+import team06.Endschalter;
 import team06.MotorSMSC;
 import team06.Variablen;
 
@@ -17,6 +18,8 @@ public class Test_Main_M3 extends Task {
 	static MotorSMSC_M3 fahrmotor1;
 	static MotorSMSC_M3 fahrmotor2;
 	
+	static Endschalter endschalter;
+	
 	int counter = 0;
 
 	public Test_Main_M3() {	
@@ -25,12 +28,12 @@ public class Test_Main_M3 extends Task {
 //		wurfmotor3 = new MotorSMSC_M3(0.001f, 5, 7, true, 8, true, 64, 5f, 3249f / 196f, 1f, 0.01f);
 //		wurfmotor4 = new MotorSMSC_M3(0.01f, 4, 6, true, 10, true, 256, 12f, 91f/1f, 1f, 0.008f);
 	
-//		fahrmotor1 = new MotorSMSC_M3(0.01f, 5, 7, true, 8, true, 256, 12f, 91f/1f, 1f, 0.008f);
-//		fahrmotor2 = new MotorSMSC_M3(0.01f, 4, 6, true, 10, true, 256, 12f, 91f/1f, 1f, 0.008f);
+		fahrmotor1 = new MotorSMSC_M3(0.01f, 5, 7, true, 8, true, 256, 12f, 91f/1f, 1f, 0.008f);
+		fahrmotor2 = new MotorSMSC_M3(0.01f, 4, 6, true, 10, true, 256, 12f, 91f/1f, 1f, 0.008f);
 
 		
-		wurfmotor3 = new MotorSMSC_M3(0.01f, 1, 3, true, 12, true, 32, 12f, 1f / 1f, 0.001f, 0.0051f);
-		wurfmotor4 = new MotorSMSC_M3(0.01f, 0, 2, true, 14, true, 32, 12f, 1f / 1f, 0.001f, 0.0051f);
+//		wurfmotor3 = new MotorSMSC_M3(0.01f, 1, 3, true, 12, true, 32, 12f, 1f / 1f, 0.001f, 0.0051f);
+//		wurfmotor4 = new MotorSMSC_M3(0.01f, 0, 2, true, 14, true, 32, 12f, 1f / 1f, 0.001f, 0.0051f);
 		
 	
 		System.out.println("Main Konstruktor gestartet");
@@ -38,89 +41,105 @@ public class Test_Main_M3 extends Task {
 
 	public void action() {
 		
-		wurfmotor3.motorstarten();
-		wurfmotor4.motorstarten();
-		
-		if(nofActivations%200==0) {
-		System.out.print("Wurfmotor 3: Umdrehungen >");
-		System.out.print(wurfmotor3.gibUmdrehungen());
-		System.out.print("        Geschwindigkeit in 1/min  >");
-		System.out.println(wurfmotor3.gibGeschwindigkeit());
-		
-		System.out.print("Wurfmotor 4: Umdrehungen >");
-		System.out.print(wurfmotor4.gibUmdrehungen());
-		System.out.print("        Geschwindigkeit in 1/min  >");
-		System.out.println(wurfmotor4.gibGeschwindigkeit());
-		}	
-		
-//		fahrmotor1.motorstarten();
-//		fahrmotor2.motorstarten();
+//		wurfmotor3.motorstarten();
+//		wurfmotor4.motorstarten();
 //		
-//		if(nofActivations%500==0) {
-//		System.out.print("Fahrmotor 1: Umdrehungen >");
-//		System.out.print(fahrmotor1.gibUmdrehungen());
+//		if(nofActivations%200==0) {
+//		System.out.print("Wurfmotor 3: Umdrehungen >");
+//		System.out.print(wurfmotor3.gibUmdrehungen());
 //		System.out.print("        Geschwindigkeit in 1/min  >");
-//		System.out.println(fahrmotor1.gibGeschwindigkeit());
+//		System.out.println(wurfmotor3.gibGeschwindigkeit());
 //		
-//		System.out.print("Fahrmotor 2: Umdrehungen >");
-//		System.out.print(fahrmotor2.gibUmdrehungen());
+//		System.out.print("Wurfmotor 4: Umdrehungen >");
+//		System.out.print(wurfmotor4.gibUmdrehungen());
 //		System.out.print("        Geschwindigkeit in 1/min  >");
-//		System.out.println(fahrmotor2.gibGeschwindigkeit());
+//		System.out.println(wurfmotor4.gibGeschwindigkeit());
 //		}	
 		
+		fahrmotor1.motorstarten();
+		fahrmotor2.motorstarten();
 		
-//		if(fahrmotor1.gibUmdrehungen() == 3 && counter == 0) {
-//			
-//			fahrmotor1.setdrehzahl(0);
-//			fahrmotor2.setdrehzahl(0);
-//			
-//			counter ++;
-//			
-//		}		
+		if(nofActivations%150==0) {
+			System.out.println("test");
+		System.out.print("Fahrmotor 1: Umdrehungen >");
+		System.out.print(fahrmotor1.gibUmdrehungen());
+		System.out.print("        Geschwindigkeit in 1/min  >");
+		System.out.println(fahrmotor1.gibGeschwindigkeit());
+		
+		System.out.print("Fahrmotor 2: Umdrehungen >");
+		System.out.print(fahrmotor2.gibUmdrehungen());
+		System.out.print("        Geschwindigkeit in 1/min  >");
+		System.out.println(fahrmotor2.gibGeschwindigkeit());
+		}	
+		
+		
+		if(fahrmotor1.gibUmdrehungen() == -3 && counter == 0) {
+			
+			fahrmotor1.setdrehzahl(0);
+			fahrmotor2.setdrehzahl(0);
+			
+			counter ++;
+			
+		}	
+		
+		
+		if(fahrmotor1.gibUmdrehungen() == -3 && counter == 1) {
+			
+			retour();
+			
+			counter ++;
+			
+		}	
 	
 	}
 	
 	public static void nullspeed() {
-		wurfmotor3.setdrehzahl(0);
-		wurfmotor4.setdrehzahl(0);
+//		wurfmotor3.setdrehzahl(0);
+//		wurfmotor4.setdrehzahl(0);
 		
-//		fahrmotor1.setdrehzahl(0);
-//		fahrmotor2.setdrehzahl(0);
+		fahrmotor1.setdrehzahl(0);
+		fahrmotor2.setdrehzahl(0);
 	}
 	
 	public static void viertelspeed() {
-		wurfmotor3.setdrehzahl((float)(50 * Math.PI));
-		wurfmotor4.setdrehzahl((float)(50 * Math.PI));
+//		wurfmotor3.setdrehzahl((float)(50 * Math.PI));
+//		wurfmotor4.setdrehzahl((float)(50 * Math.PI));
 		
-//		fahrmotor1.setdrehzahl((float)(1 * Math.PI));
-//		fahrmotor2.setdrehzahl((float)(1 * Math.PI));
+		fahrmotor1.setdrehzahl((float)(-1 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(1 * Math.PI));
 	}
 	
 	public static void halbspeed() {
-		wurfmotor3.setdrehzahl((float)(100 * Math.PI));
-		wurfmotor4.setdrehzahl((float)(100 * Math.PI));
+//		wurfmotor3.setdrehzahl((float)(100 * Math.PI));
+//		wurfmotor4.setdrehzahl((float)(100 * Math.PI));
 		
-//		fahrmotor1.setdrehzahl((float)(2 * Math.PI));
-//		fahrmotor2.setdrehzahl((float)(2 * Math.PI));
+		fahrmotor1.setdrehzahl((float)(-2 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(2 * Math.PI));
 	}
 	
 	public static void maxspeed() {
-		wurfmotor3.setdrehzahl((float)(200 * Math.PI));
-		wurfmotor4.setdrehzahl((float)(200 * Math.PI));
+//		wurfmotor3.setdrehzahl((float)(200 * Math.PI));
+//		wurfmotor4.setdrehzahl((float)(200 * Math.PI));
 		
-//		fahrmotor1.setdrehzahl((float)(4 * Math.PI));
-//		fahrmotor2.setdrehzahl((float)(4 * Math.PI));
+		fahrmotor1.setdrehzahl((float)(-4 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(4 * Math.PI));	
+			
+		}
+	
+	public static void retour() {
+		fahrmotor1.setdrehzahl((float)(0.5 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(-0.5 * Math.PI));
 	}
 	
 	public static void linkskurve() {
 		fahrmotor1.setdrehzahl((float)(0.5 * Math.PI));
-		fahrmotor2.setdrehzahl((float)(-0.5 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(0.5 * Math.PI));
 		
 	}
 	
 	public static void rechtskurve() {
 		fahrmotor1.setdrehzahl((float)(-0.5 * Math.PI));
-		fahrmotor2.setdrehzahl((float)(0.5 * Math.PI));
+		fahrmotor2.setdrehzahl((float)(-0.5 * Math.PI));
 		
 	}
 	
--- src/team06/testprogramme/TPUDIO.java
+++ src/team06/testprogramme/TPUDIO.java
@@ -5,23 +5,88 @@ import ch.ntb.inf.deep.runtime.mpc555.driver.TPU_DIO;
 import ch.ntb.inf.deep.runtime.ppc32.Task;
 
 public class TPUDIO extends Task {
-	public final byte ledPin = 2;
+	public final byte ledPin = 3;
 	// Pin fuer LED festlegen
-	TPU_DIO led;
+	TPU_DIO leda, ledb, ledc, ledd, lede, ledf, ledg, leddp;
+	int counter = 0;
 
 	public TPUDIO() {
 
-		led = new TPU_DIO(true, ledPin, true); // Pin initialisieren
-		led.set(false); // LED ausschalten
+		leda = new TPU_DIO(false, 11, true); // Pin initialisieren
+		ledb = new TPU_DIO(false, 9, true);
+		ledc = new TPU_DIO(false, 5, true);
+		ledd = new TPU_DIO(false, 3, true);
+		lede = new TPU_DIO(false, 1, true);
+		ledf = new TPU_DIO(false, 13, true);
+		ledg = new TPU_DIO(false, 15, true);
+		leddp = new TPU_DIO(false, 7, true);
+
+		leda.set(false); // LED ausschalten
+		ledb.set(false);
+		ledc.set(false);
+		ledd.set(false); // LED ausschalten
+		lede.set(false);
+		ledf.set(false);
+		ledg.set(false); // LED ausschalten
+		leddp.set(false);
+
 	}
 
 	public void action() {
-		led.set(!led.get()); // LED “toggeln”
+		leddp.set(!leddp.get()); // LED “toggeln”
+		// led.set(!led.get()); // LED “toggeln”
+
+		// kleines N (n)
+		if (counter == 0) {
+			leda.set(true); // LED ausschalten
+			ledb.set(true);
+			ledc.set(false);
+			ledd.set(true); // LED ausschalten
+			lede.set(false);
+			ledf.set(true);
+			ledg.set(false); // LED ausschalten
+			leddp.set(true);
+		}
+        
+		// E
+		if (counter == 1) {
+			leda.set(false); // LED ausschalten
+			ledb.set(true);
+			ledc.set(true);
+			ledd.set(false); // LED ausschalten
+			lede.set(false);
+			ledf.set(false);
+			ledg.set(false); // LED ausschalten
+			leddp.set(true);
+		}
+
+		// F
+		if (counter == 2) {
+			leda.set(false); // LED ausschalten
+			ledb.set(true);
+			ledc.set(true);
+			ledd.set(true); // LED ausschalten
+			lede.set(false);
+			ledf.set(false);
+			ledg.set(false); // LED ausschalten
+			leddp.set(true);
+		}
+
+//		if (counter > 3) {
+//			leddp.set(!leddp.get()); // LED “toggeln”
+//
+//		}
+		counter++;
+
+		if (counter == 5) {
+			counter = 0;
+		}
+
 	}
 
 	static { // Klassenkonstruktor
 		Task t = new TPUDIO(); // Task erzeugen
-		t.period = 500; // Task-Periode festlegen
+		t.period = 200; // Task-Periode festlegen
 		Task.install(t); // Task installieren
 	}
 }
