073d3b2345af1a9fa3ac123507f79ce4a9a035d3
elevator4000 <loris-graf@bluewin.ch>
2019-05-27 18:01:34 +0200

    Update Pos_Wechsel_V2.java

--- src/team02/vorlagen/Pos_Wechsel_V2.java
+++ src/team02/vorlagen/Pos_Wechsel_V2.java
@@ -21,7 +21,7 @@ public class Pos_Wechsel_V2 {
         NachGegner11, NachGegner12, NachGegner13,					// Zustand NACH dem passieren einer Gegnerposition (Gegner von Weg1)
         NachGegner24, NachGegner23, NachGegner22, NachGegner21,		// Zustand NACH dem passieren einer Gegnerposition (Gegner von Weg2)
         
-        AnPosA,AnPosB, AnPosC, AnPosD,								// Zielposition erreicht (ACHTUNG: Roboter kann noch in einer Drehbewegung sein -> kein Stillstand garantiert )
+        AnPosB, AnPosC, AnPosD,								// Zielposition erreicht (ACHTUNG: Roboter kann noch in einer Drehbewegung sein -> kein Stillstand garantiert )
         
         TraverseZuEbene2, TraverseZuEbene3,							// Ebene wird gewechselt
         
@@ -44,22 +44,13 @@ public class Pos_Wechsel_V2 {
     private static double Distanz_G4 		= 0.120;
     private static double Distanz_Linie 	= 0.260;
     
-    
-    /**
-     * Fahre autonom zu WurfPosition1
-     */
-    public static void fahre_zu_PosA()
-    {
-        zustand = Zustand.Pos0toA;
-    }
-    
     /**
      * Fahre autonom zu WurfPosition1
      */
     public static void fahre_zu_Pos1()
     {
     	ersteFahrt = true;
-        zustand = Zustand.PosAtoB;
+        zustand = Zustand.Pos0toA;
     }
     
     /**
@@ -69,15 +60,6 @@ public class Pos_Wechsel_V2 {
     {
     	ersteFahrt = false;
     	zustand = Zustand.PosBtoC;
-    }
-    
-    /**
-     *  Wurde WurfPositionA erreicht?
-     * return WurfPositionA erreicht
-     */
-    public static boolean posA_erreicht()
-    {
-        return (zustand == Zustand.AnPosA)&&(!Systeme.bewegungsSystem.istInBewegung());
     }
     
     /**
@@ -128,7 +110,7 @@ public class Pos_Wechsel_V2 {
 			case Pos0toA:	// Fahre von Position 0 zu Position A
 			{
 				Systeme.bewegungsSystem.folgeLinieBisWandRueckwaerts();
-				zustand = Zustand.AnPosA;
+				zustand = Zustand.PosAtoB;
 				break;
 			}
     		case PosAtoB:	// Fahre von Position A zu Position B
