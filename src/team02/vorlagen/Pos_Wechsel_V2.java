a6a77faa7179dbf6096ee00616e85a79319c1aa7
elevator4000 <loris-graf@bluewin.ch>
2019-06-14 16:35:17 +0200

    Update Pos_Wechsel_V2.java

--- src/team02/vorlagen/Pos_Wechsel_V2.java
+++ src/team02/vorlagen/Pos_Wechsel_V2.java
@@ -37,11 +37,11 @@ public class Pos_Wechsel_V2 {
     
     private static double Distanz_Wand_Abstand 	= 0.015;    
     
-    private static double Distanz_G1 		= 0.100; 
+    private static double Distanz_G1 		= 0.105; 
     private static double Distanz_G1_kurz 	= Distanz_G1-Distanz_Wand_Abstand; 
-    private static double Distanz_G2 		= 0.148;
-    private static double Distanz_G3 		= 0.100;
-    private static double Distanz_G4 		= 0.160;
+    private static double Distanz_G2 		= 0.167;
+    private static double Distanz_G3 		= 0.080;
+    private static double Distanz_G4 		= 0.080;
     private static double Distanz_Linie 	= 0.260;
     
     /**
@@ -234,7 +234,7 @@ public class Pos_Wechsel_V2 {
             {
 	            if (Systeme.gegnerSystem.warGegnerLinks())
 	            {
-	                Systeme.bewegungsSystem.folgeLinieBisDistanzUngebremst(true, Distanz_G3);
+	                Systeme.bewegungsSystem.folgeLinieBisDistanzUngebremst(true, Distanz_G2);
 	                zustand = Zustand.NachGegner22;
 	            }
 	            else
